7c82c85037a17232384af0a51b48082f40359e30
516108736 <516108736@qq.com>
2020-07-09 18:40:52 +0800

    1840

--- cmd/cluster/run_cluster.sh
+++ cmd/cluster/run_cluster.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
 if [ x"$1" = x ]; then
-    configPath="../../mainnet/singularity/cluster_config_template.json"
-    echo "not set config. use default config . configPath="$configPath
+	if [ -f "../../mainnet/singularity/cluster_config_template.json" ]; then
+		configPath="../../mainnet/singularity/cluster_config_template.json"
+	elif [ -f "./cluster_config_template.json" ]; then
+		configPath="./cluster_config_template.json"
+	else
+		echo "config not set"
+		exit
+	fi
 else
-    configPath=$1
-    echo "set config, configPath="$configPath
+	configPath=$1
 fi
-
-slaveInfo=`grep -Po 'ID[" :]+\K[^"]+' $configPath | grep S`
+echo "configPath="$configPath
+slaveInfo=$(grep -Po 'ID[" :]+\K[^"]+' $configPath | grep S)
 
 # start slave
-for value in $slaveInfo
-do
-   sLog=`pwd`/${value}.log
-	 cmd="./cluster --cluster_config "${configPath}" --service "${value}">> "$sLog" 2>&1 &"
-	 eval $cmd
-	 pid=$!
-	 echo "Start "${value}"     successfull pid="$pid" logFile=$sLog"
+for value in $slaveInfo; do
+	sLog=$(pwd)/${value}.log
+	cmd="./cluster --cluster_config "${configPath}" --service "${value}">> "$sLog" 2>&1 &"
+	eval $cmd
+	pid=$!
+	echo "Start "${value}"     successfull pid="$pid" logFile=$sLog"
 done
 
 sleep 5s
 
 # start master
-mLog=`pwd`/master.log
+mLog=$(pwd)/master.log
 cmd="./cluster --cluster_config "${configPath}" >>$mLog 2>&1 &"
 eval $cmd
 pid=$!
