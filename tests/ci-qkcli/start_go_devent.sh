05ef53793d076e8c7e5b31f1670fe859a13d517b
516108736 <516108736@qq.com>
2020-08-05 18:59:08 +0800

    1858

--- tests/ci-qkcli/start_go_devent.sh
+++ tests/ci-qkcli/start_go_devent.sh
@@ -1,4 +1,4 @@
 
-./run_cluster.sh ./cluster_config.json
+chmod +x ./run_cluster.sh  && ./run_cluster.sh ./cluster_config.json
 sleep 5
 curl -X POST -H 'content-type: application/json' --data '{"jsonrpc":"2.0","method":"setMining","params":[true],"id":0}' http://127.0.0.1:38491
